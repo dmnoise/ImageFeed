d04dcabe55f5c6e651c32ae494e53a51189cdc20
dmnoise <svaston@hotmail.com>
2025-03-08 13:15:11 +0300

    Исправил размер лого launchScreen, отступ от статус бара и скрыл navigationBar

--- ImageFeed/ImageList/ImagesListViewController.swift
+++ ImageFeed/ImageList/ImagesListViewController.swift
@@ -31,6 +31,11 @@ final class ImagesListViewController: UIViewController {
         tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == SingleImageViewController.identifier {
             guard
