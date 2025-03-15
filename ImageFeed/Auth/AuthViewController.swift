528986f9311d39697f6c7c0c4e31c643c01b3668
dmnoise <svaston@hotmail.com>
2025-03-15 13:18:03 +0300

    Добавил экран авторизации, убрал лишний нав. контроллер

---
+++ ImageFeed/Auth/AuthViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AuthViewController.swift
+//  ImageFeed
+//
+//  Created by Dmitriy Noise on 15.03.2025.
+//
+
+import UIKit
+
+class AuthViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
